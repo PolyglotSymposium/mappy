50d3248b6358884562075221e6a42da86c785144
Michael Gilliland <mjg.py3@gmail.com>
2016-01-03 13:59:12 -0500

    Parse most core primitives... Still need whitespace parses and apply

--- src/Mappy.hs
+++ src/Mappy.hs
@@ -1,7 +1,39 @@
 module Mappy where
 
+import qualified Data.Map as M
 import Text.ParserCombinators.Parsec
 
-data Expression = MappyMap deriving (Eq, Show)
+data Expression =
+  MappyMap (M.Map Expression Expression)
+  | MappyKeyword String
+  | MappyNamedValue String
+  deriving (Eq, Show, Ord)
 
-expression = undefined
+whiteSpace :: Parser String
+whiteSpace = many (oneOf " \n\r\t") <?> "whitespace"
+
+pairs :: Parser (M.Map Expression Expression)
+pairs = do
+  exprs <- expression `sepEndBy` whiteSpace
+  return $ toMap exprs
+    where
+    toMap [] = M.empty
+    toMap (k:v:rest) = M.insert k v $ toMap rest
+
+map' :: Parser Expression
+map' = MappyMap <$> between
+  (char '(')
+  (char ')')
+  pairs
+
+identifier :: Parser String
+identifier = many1 $ letter <|> digit <|> oneOf "_/-+<>!@#$%^&*\\;'\",.?"
+
+keyword :: Parser Expression
+keyword = char ':' >> (MappyKeyword <$> identifier)
+
+namedValue :: Parser Expression
+namedValue = MappyNamedValue <$> identifier
+
+expression :: Parser Expression
+expression = map' <|> keyword <|> namedValue
--- test/MappySpec.hs
+++ test/MappySpec.hs
@@ -1,20 +1,68 @@
 module MappySpec (main, spec) where
 
+import qualified Data.Map as M
+import Text.ParserCombinators.Parsec (parse)
+import Data.Either (isLeft)
+
 import Test.Hspec
 import Test.QuickCheck
 
 import Mappy
-import Text.ParserCombinators.Parsec (parse)
+
+data ArbitraryValidKeywordName =
+  ValidKeywordName String
+  deriving (Show)
+
+instance Arbitrary ArbitraryValidKeywordName where
+  arbitrary = do
+    let validChars = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "_/-+<>!@#$%^&*\\;'\",.?"
+    text <- arbitrary
+    firstChar <- elements validChars
+    let filtered = filter (`elem` validChars) text
+    return $ ValidKeywordName (firstChar:filtered)
 
 spec :: Spec
 spec = do
-  describe "parsing" $ do
-    describe "expressions" $ do
-      let parseExpression = parse expression ""
+  describe "the text for the expression" $ do
+    let parseExpression = parse expression ""
 
-      describe "a single map" $ do
-        it "works" $ do
-          parseExpression "()" `shouldBe` Right MappyMap
+    describe "when parsing maps" $ do
+      describe "the empty map" $ do
+        it "parses correctly" $ do
+          parseExpression "()" `shouldBe` Right (MappyMap M.empty)
 
-main :: IO ()
+      describe "a map containing" $ do
+        describe "a single association of keywords" $ do
+          it "parses correctly" $ do
+            parseExpression "(:a :b)" `shouldBe` Right (MappyMap $ M.singleton (MappyKeyword "a") (MappyKeyword "b"))
+
+        describe "a single association of maps" $ do
+          it "parses correctly" $ do
+            parseExpression "(() ())" `shouldBe` Right (MappyMap $ M.singleton (MappyMap M.empty) (MappyMap M.empty))
+
+        describe "a single association of a map to a keyword" $ do
+          it "parses correctly" $ do
+            parseExpression "(() :foobar)" `shouldBe` Right (MappyMap $ M.singleton (MappyMap M.empty) (MappyKeyword "foobar"))
+
+    describe "a single keyword" $ do
+      it "parses correctly" $ do
+        property $ \(ValidKeywordName text) -> parseExpression (':':text) `shouldBe` Right (MappyKeyword text)
+
+    describe "a single named value" $ do
+      it "parses correctly" $ do
+        property $ \(ValidKeywordName text) -> parseExpression text `shouldBe` Right (MappyNamedValue text)
+
+    describe "when parsing various empty values" $ do
+      describe "the \"empty keyword\"" $ do
+        it "is not a valid expression" $ do
+          parseExpression ":" `shouldSatisfy` isLeft
+
+      describe "the empty string" $ do
+        it "is not a valid expression" $ do
+          parseExpression "" `shouldSatisfy` isLeft
+
+      describe "the empty application" $ do
+        it "is not a valid expression" $ do
+          parseExpression "[]" `shouldSatisfy` isLeft
+
 main = putStrLn "Hello there"
