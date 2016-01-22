afcebe68df24a793607545aeff78b6ad7f802ae6
Michael Gilliland <mjg.py3@gmail.com>
2016-01-22 17:00:29 -0500

    Fix spacing between params in pretty printer (Minor)

--- src/Language/Ast.hs
+++ src/Language/Ast.hs
@@ -49,5 +49,5 @@ pretty (MappyNamedValue name) = name
 pretty (MappyApp fn args) = "[" ++ intercalate " " (pretty fn:map pretty args) ++ "]"
 pretty (MappyMap (StandardMap map')) = "(" ++ intercalate " " (map (\(k, v) -> pretty k ++ " " ++ pretty v) $ M.toList map') ++ ")"
 pretty (MappyMap (IoMap _)) = "__prim_io_map"
-pretty (MappyLambda args body) = "\\" ++ intercalate "" (map pretty args) ++ " -> " ++ pretty body
+pretty (MappyLambda args body) = "\\" ++ intercalate " " (map pretty args) ++ " -> " ++ pretty body
 pretty (MappyClosure args body _) = "#closure[...]#" ++ pretty (MappyLambda args body)
